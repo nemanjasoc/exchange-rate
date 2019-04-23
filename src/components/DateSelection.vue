6202e77f283a918eb83741c9fd0a5e17f95491fa
Nemanja Soc <nemanjasoc@gmail.com>
2019-04-23 12:40:41 +0200

    showed  calendar

--- src/components/DateSelection.vue
+++ src/components/DateSelection.vue
@@ -1,15 +1,22 @@
 <template>
 	<div class="date-selection">
-		
+		<app-datepicker></app-datepicker>
 	</div>
 </template>
 
 <script scoped>
+	import Datepicker from 'vuejs-datepicker';
+	export default {
+		components: {
+			appDatepicker: Datepicker
+		}
+	}
 	
 </script>
 
 <style>
 	.date-selection {
-
+		display: flex;
+		justify-content: center;
 	}
 </style>
